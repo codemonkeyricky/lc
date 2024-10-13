65dc058fbfa96e83c3d1eba8b5d8afe146c09855
Richard <code.monkey.ricky@gmail.com>
2024-10-13 13:20:36 -0700

    wip

--- 3320.cpp
+++ 3320.cpp
@@ -46,10 +46,94 @@ using vi = vector<int>;
 using vvi = vector<vector<int>>;
 using vll = vector<long long>;
 using vvll = vector<vector<long long>>;
+using ll = long long;
+
+class Solution {
+
+    /*
+     * dp[[k][score]
+     * number of ways for bob to win for a given score at k
+     */
+
+    int dfs(string& s, int k, char p, int score) {
+        int n = s.size();
+
+        if (k < 0)
+            return score > 0; /// TODO???
+
+        /*
+         * F > E
+         * W > F
+         * E > W
+         */
+
+        ll rv = 0;
+        switch (s[k]) {
+        case 'F': {
+            /* Bob uses F
+             * Same - pass score along
+             */
+
+            /*
+             * Bob uses W
+             * Bob wins - can also account for lowered score
+             */
+
+            /*
+             * Bob loses with E
+             * Bob need to increase score
+             */
+
+            if (p != 'F')
+                rv += dfs(s, k - 1, 'F', score);
+            if (p != 'W')
+                rv += dfs(s, k - 1, 'W', score - 1);
+            if (p != 'E')
+                rv += dfs(s, k - 1, 'E', score + 1);
+
+        } break;
+        case 'W': {
+
+            /*
+             * F > E
+             * W > F
+             * E > W
+             */
+
+            if (p != 'F') // loses
+                rv += dfs(s, k - 1, 'F', score + 1);
+            if (p != 'W') // same
+                rv += dfs(s, k - 1, 'W', score);
+            if (p != 'E') // wins
+                rv += dfs(s, k - 1, 'E', score - 1);
+        } break;
+        case 'E': {
+            if (p != 'F') // wins
+                rv += dfs(s, k - 1, 'F', score - 1);
+            if (p != 'W') // loses
+                rv += dfs(s, k - 1, 'W', score + 1);
+            if (p != 'E') // same
+                rv += dfs(s, k - 1, 'E', score);
+        } break;
+        }
+
+        return rv;
+    }
+
+  public:
+    int countWinningSequences(string s) {
+        int n = s.size();
+        int rv = 0;
+        for (auto score = 1; score <= n; ++score)
+            rv += dfs(s, n - 1, -1, score);
+        return rv;
+    }
+};
 
 int main() {
     Solution sol;
     int r;
 
+    r = sol.countWinningSequences("FFF");
     cout << r << endl;
 }
