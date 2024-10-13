177668841c36a452cc4ac26773e43f5a01251294
Richard <code.monkey.ricky@gmail.com>
2024-10-12 23:37:03 -0700

    wip

--- 3317.cpp
+++ 3317.cpp
@@ -46,15 +46,53 @@ using vi = vector<int>;
 using vvi = vector<vector<int>>;
 using vll = vector<long long>;
 using vvll = vector<vector<long long>>;
+using ll = long long;
 
 class Solution {
+
+    long long n_choose_k(long long n, long long k) {
+        if (k == 0)
+            return 1;
+        return (n * n_choose_k(n - 1, k - 1)) / k;
+    }
+
   public:
-    int numberOfWays(int n, int x, int y) {}
+    int numberOfWays(int n, int x, int y) {
+        vvi dp(n + 1, vi(x + 1));
+
+        /* dp[n][x] is n performers in x stages */
+
+        for (auto i = 1; i <= x; ++i)
+            dp[0][i] = 1;
+
+        for (auto nn = 1; nn <= n; ++nn) {
+            /*
+             * dp[nn][xx] adds a new performer to dp[nn-1][xx].
+             * New performer can join any of the stages from 1 to xx.
+             */
+            for (auto xx = 1; xx <= x; ++xx) {
+                dp[nn][xx] += dp[nn - 1][xx] * xx;
+                volatile int dummy = 0;
+            }
+        }
+
+        // ll rv = 0;
+        // for (auto xx = 1; xx <= x; ++xx) {
+        //     /* need to spread xx across x stages using nCk */
+        //     rv = rv + dp[n][xx] * n_choose_k(x, xx); /* TODO: x choose xx */
+        // }
+
+        return dp[n][x] * y;
+    }
 };
 
 int main() {
     Solution sol;
     int r;
 
+    // r = sol.numberOfWays(3, 3, 4);
+    // cout << r << endl;
+
+    r = sol.numberOfWays(5, 2, 1);
     cout << r << endl;
 }
