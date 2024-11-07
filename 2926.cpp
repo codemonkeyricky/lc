9ca23c99b09fd714043005ee592bcbfae9bdb5c3
Richard <code.monkey.ricky@gmail.com>
2024-11-07 08:06:47 -0800

    wip

--- 2926.cpp
+++ 2926.cpp
@@ -1,123 +1,62 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-#include <stack>
-#include <queue>
 #include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
 #include <unordered_map>
 #include <unordered_set>
-#include <map>
-#include <set>
-#include <bitset>
-#include <numeric>
-#include <cmath>
+#include <vector>
 
 using namespace std;
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
 };
 
-TreeNode *recurse(vector<int> &tree, int k)
-{
+TreeNode* recurse(vector<int>& tree, int k) {
     if (k >= tree.size() || tree[k] == -1)
         return nullptr;
 
-    return new TreeNode(tree[k], recurse(tree, k * 2 + 1), recurse(tree, k * 2 + 2));
+    return new TreeNode(tree[k], recurse(tree, k * 2 + 1),
+                        recurse(tree, k * 2 + 2));
 }
 
-TreeNode *populate(vector<int> &tree)
-{
-    return recurse(tree, 0);
-}
+TreeNode* populate(vector<int>& tree) { return recurse(tree, 0); }
 
-class Solution
-{
-    vector<long long> lazy, tree;
-
-    void push(int v)
-    {
-        tree[v * 2] += lazy[v];
-        lazy[v * 2] += lazy[v];
-        tree[v * 2 + 1] += lazy[v];
-        lazy[v * 2 + 1] += lazy[v];
-        lazy[v] = 0;
-    }
-
-    void update(int v, int tl, int tr, int l, int r, long long addend)
-    {
-        if (l > r)
-            return;
-        if (l == tl && tr == r)
-        {
-            tree[v] += addend;
-            lazy[v] += addend;
-        }
-        else
-        {
-            push(v);
-            int tm = (tl + tr) / 2;
-            update(v * 2, tl, tm, l, min(r, tm), addend);
-            update(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, addend);
-            tree[v] = max(tree[v * 2], tree[v * 2 + 1]);
-        }
-    }
-
-    long long query(int v, int tl, int tr, int l, int r)
-    {
-        if (l > r)
-            return -1e9 - 2;
-        if (l == tl && tr == r)
-            return tree[v];
-        push(v);
-        int tm = (tl + tr) / 2;
-        return max(query(v * 2, tl, tm, l, min(r, tm)),
-                   query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
-    }
-
-public:
-    long long maxBalancedSubsequenceSum(vector<int> &nums)
-    {
+class Solution {
+  public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
         int n = nums.size();
-        set<int> uniq;
-        for (auto i = 0; i < n; ++i)
-            uniq.insert(nums[i] - i);
 
-        unordered_map<int, int> lookup;
-        int k = 0;
-        for (auto &u : uniq)
-            lookup[u] = k++;
-
-        vector<int> nums2;
-        for (auto i = 0; i < n; ++i)
-            nums2.push_back(lookup[nums[i] - i]);
-
-        lazy = tree = vector<long long>(n * 4);
-
-        for (auto i = 0; i < n; ++i)
-            update(1, 0, n - 1, i, i, -1e9 - 1);
-
-        for (auto i = 0; i < n; ++i)
-        {
-            auto k = nums2[i];
-            auto v = query(1, 0, n - 1, 0, k);
-            auto reset = query(1, 0, n - 1, k, k);
-            update(1, 0, n - 1, k, k, -reset);
-            update(1, 0, n - 1, k, k, (v < 0 ? 0 : v) + nums[i]);
+        // vector<array<int, 2>> nums2;
+        for (auto i = 0; i < n; ++i) {
+            // nums2.push_back({nums[i] - i, i}); ///< for sorting / index
         }
 
-        return query(1, 0, n - 1, 0, n - 1);
+        // set<array<int, 2>> dp; ///< for sorting / sum
+
+        for (auto i = 0; i < n; ++i) {
+            // array<int, 2> target = {nums2[i], nums2[i]};
+            // auto it = set.lower_bound(dp.begin(), dp.end(), target);
+            // if (it == dp.end()) {
+            //     set.insert(target);
+            // }
+        }
     }
 };
 
-int main()
-{
+int main() {
     Solution sol;
     int r;
 
