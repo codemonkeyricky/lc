ef03aa10ce497bf9a3b61713e1bb59dfe023dcd4
Richard <code.monkey.ricky@gmail.com>
2024-11-21 21:55:04 -0800

    wip

--- 212.cpp
+++ 212.cpp
@@ -48,46 +48,70 @@ using vll = vector<long long>;
 using vvll = vector<vector<long long>>;
 
 class Solution {
+
     using vi = vector<int>;
     using vvi = vector<vector<int>>;
     using vvc = vector<vector<char>>;
     using vs = vector<string>;
-    bool dfs(vvc& b, string& w, int i, int j, int kk, vvi& seen) {
+
+    struct Trie {
+        array<Trie*, 26> letters = {};
+        bool ending = false;
+    };
+
+    Trie root;
+
+    void dfs(vvc& b, int i, int j, Trie* trie, string& s, vvi& seen,
+             set<string>& rv) {
         int m = b.size(), n = b[0].size();
 
-        if (w[kk] != b[i][j])
-            return false;
-        if (kk + 1 == w.size())
-            return true;
-        bool rv = false;
+        if (trie->letters[b[i][j] - 'a'] == nullptr)
+            return;
+
+        trie = trie->letters[b[i][j] - 'a'];
+        s.push_back(b[i][j]);
+
+        if (trie->ending) {
+            rv.insert(s);
+            s.pop_back();
+            return;
+        }
+
         vi offset = {0, -1, 0, 1, 0};
         seen[i][j] = true;
         for (auto k = 0; k < 4; ++k) {
-            auto ni = i + offset[k];
+            auto ni = i + offset[k + 0];
             auto nj = j + offset[k + 1];
             if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
                 if (!seen[ni][nj]) {
-                    rv |= dfs(b, w, ni, nj, kk + 1, seen);
+                    dfs(b, ni, nj, trie, s, seen, rv);
                 }
             }
         }
         seen[i][j] = false;
-        return rv;
+        s.pop_back();
     }
 
   public:
     vector<string> findWords(vector<vector<char>>& b, vector<string>& words) {
         int m = b.size(), n = b[0].size();
 
+        /* construct trie */
+        for (auto& w : words) {
+            auto c = &root;
+            for (auto i = 0; i < w.size(); ++i) {
+                if (c->letters[w[i] - 'a'] == nullptr)
+                    c->letters[w[i] - 'a'] = new Trie();
+                c = c->letters[w[i] - 'a'];
+            }
+            c->ending = true;
+        }
+
         set<string> rv;
-        for (auto k = 0; k < words.size(); ++k) {
-            for (auto i = 0; i < m; ++i) {
-                for (auto j = 0; j < n; ++j) {
-                    auto seen = vvi(12, vi(12));
-                    if (dfs(b, words[k], i, j, 0, seen)) {
-                        rv.insert(words[k]);
-                    }
-                }
+        for (auto i = 0; i < m; ++i) {
+            for (auto j = 0; j < n; ++j) {
+                auto seen = vvi(12, vi(12));
+                dfs(b, i, j, &root, string() = {}, seen, rv);
             }
         }
         return vector<string>(rv.begin(), rv.end());
