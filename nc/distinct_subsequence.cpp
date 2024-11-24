6fe1997da142596c6c4a4dba65f4cbd8103bed07
Richard <code.monkey.ricky@gmail.com>
2024-11-23 17:20:07 -0800

    wip

--- nc/distinct_subsequence.cpp
+++ nc/distinct_subsequence.cpp
@@ -75,8 +75,24 @@ class Solution {
   public:
     int numDistinct(string s, string t) {
         int m = s.size(), n = t.size();
-        dp = vector<vector<int>>(m, vector<int>(n));
-        return dfs(s, t, 0, 0);
+        dp = vector<vector<int>>(m + 1, vector<int>(n + 1));
+
+        // return dfs(s, t, 0, 0);
+
+        /* any j of size 0 is true */
+        for (auto i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+
+        for (auto i = 1; i <= m; ++i) {
+            for (auto j = 1; j <= n; ++j) {
+                dp[i][j] += dp[i - 1][j];
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] += dp[i - 1][j - 1];
+            }
+        }
+
+        return dp[m][n];
     }
 };
 
